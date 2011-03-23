168bd6af5ec64391c21da86b9ebeb0d755bc1623
Brett Graham <graham@rowland.harvard.edu>
2011-03-23 10:56:40 -0400

    adding mwk to pickle file

---
+++ mwk_converters/mwk_to_pickle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import logging, os, sys
+
+import json
+import cPickle as pickle
+
+import mworks.data as mwk
+
+logging.basicConfig(level=logging.DEBUG)
+
+eventsBlacklist = ['#announceCurrentState','#codec', '#systemEvent', '#components', '#termination']
+
+# parse command line arguments
+logging.debug("Parsing command line arguments")
+if len(sys.argv) == 3:
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+elif len(sys.argv) == 2:
+    inFile = sys.argv[1]
+    outFile = '%s.p' % os.path.splitext(os.path.basename(inFile))[0]
+else:
+    print "Usage: %s input_mwk_file (output_h5_file)" % __file__
+    sys.exit(1)
+
+# open up and read mwks file
+logging.debug("opening and reading mwks file: %s" % inFile)
+m = mwk.MWKFile(inFile)
+m.open()
+# fix codec
+c = m.codec
+c[0], c[1], c[2], c[3] = ('#codec', '#systemEvent', '#components', '#termination')
+codec = m.codec
+revCodec = m.reverse_codec
+
+# open file for writing
+oFile = open(outFile, 'w')
+
+# add data to file
+events = m.get_events()
+codeBlacklist = [revCodec[e] for e in eventsBlacklist]
+pData = {'codec': codec, 'revCodec': revCodec, 'events': []}
+for e in events:
+    if e.code in codeBlacklist:
+        continue
+    pData['events'].append({'code': e.code, 'time': e.time, 'value': json.dumps(e.value)})
+pickle.dump(pData, oFile, protocol=2)
+
+# Close the file
+oFile.close()
+
+# close mworks file
+m.close()
+
+# exit nicely
+sys.exit(0)
